d11b34aee40006ccf12be43cbc9b7b2c8e7f3360
Hamid Nur <hamidsharif2001@gmail.com>
2022-03-21 17:16:41 -0400

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,14 @@ import './Header.css'
 function Header() {
   return (
     <div className='header'>
-      Header
+      <img 
+        className='header_logo'
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+      />
+      <div className='header_search'>
+        <input className='header_searchInput' type='text'/>
+        {/* logo */}
+      </div>
     </div>
   )
 }
