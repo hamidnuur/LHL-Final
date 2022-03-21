d225d914c3cc9c8caafef6c2757d243a4ef612fc
Hamid Nur <hamidsharif2001@gmail.com>
2022-03-21 16:43:50 -0400

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCpkTdEJFuTVflLMLbKPWbjbg0Gjdn14tg",
+  authDomain: "lhl-final-82e6c.firebaseapp.com",
+  projectId: "lhl-final-82e6c",
+  storageBucket: "lhl-final-82e6c.appspot.com",
+  messagingSenderId: "917240882484",
+  appId: "1:917240882484:web:24001d8d0e1d4a16273161"
+};
