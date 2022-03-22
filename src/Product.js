5a68f4959075f20ec56ca177f8758633972f8e54
Hamid Nur <hamidsharif2001@gmail.com>
2022-03-21 22:26:55 -0400

    build product component

--- src/Product.js
+++ src/Product.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import "./Product.css";
+
 
 function Product() {
+  
   return (
-    <div>Product</div>
-  )
+    <div className="product">
+
+      <div className="product__info">
+
+        <p className="product__price">
+          <small>$</small>
+          <strong>29.99</strong>
+        </p>
+        <div className="product__rating">
+
+        </div>
+      </div>
+
+      <img src='' alt="" />
+
+      <button>Add to Basket</button>
+    </div>
+  );
 }
 
-export default Product
+export default Product;
