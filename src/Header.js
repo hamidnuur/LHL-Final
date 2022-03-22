700b975107ce4e677fd0fa9b2ca67c1451c4f3bf
Hamid Nur <hamidsharif2001@gmail.com>
2022-03-21 21:34:32 -0400

    finish home css styling

--- src/Header.js
+++ src/Header.js
@@ -1,44 +1,39 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 function Header() {
   return (
     <div className='header'>
       <img 
-        className='header_logo'
+        className='header__logo'
         src="images/sonder-logo.png"
       />
 
-      <div className='header_search'>
-        <input className='header_searchInput' type='text'/>
-        {/* logo */}
+      <div className='header__search'>
+        <input className='header__searchInput' type='text'/>
+        <SearchIcon className='header__searchIcon' />
       </div>
 
-      <div className='header_nav'>
-        <div className='header_option'>
-          <span classname='header_optionLineOne'>
-            Hello User
-          </span>
-          <span classname='header_optionLineTwo'>
+      <div className='header__nav'>
+        <div className='header__option'>
+          <span className='header__optionLineTwo'>
             Sign In
           </span>
         </div>
-        <div className='header_option'>
-          <span classname='header_optionLineOne'>
-            returns
-          </span>
-          <span classname='header_optionLineTwo'>
-            & orders
+
+        <div className='header__option'>
+          <span className='header__optionLineTwo'>
+            Orders
           </span>
         </div>
-        <div className='header_option'>
-          <span classname='header_optionLineOne'>
-            Your
-          </span>
-          <span classname='header_optionLineTwo'>
-            Prime
-          </span>
+
+        <div className='header__optionBasket'>
+          <ShoppingBasketIcon />
+          <span className='header__optionLineTwo header__basketCount' >0</span>
         </div>
+
       </div>
 
     </div>
