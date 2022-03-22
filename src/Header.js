23662c044acd8d5503168ff4784ca2de46ce188e
Hamid Nur <hamidsharif2001@gmail.com>
2022-03-21 20:14:19 -0400

    Building header divs

--- src/Header.js
+++ src/Header.js
@@ -8,10 +8,39 @@ function Header() {
         className='header_logo'
         src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
       />
+
       <div className='header_search'>
         <input className='header_searchInput' type='text'/>
         {/* logo */}
       </div>
+
+      <div className='header_nav'>
+        <div className='header_option'>
+          <span classname='header_optionLineOne'>
+            Hello User
+          </span>
+          <span classname='header_optionLineTwo'>
+            Sign In
+          </span>
+        </div>
+        <div className='header_option'>
+          <span classname='header_optionLineOne'>
+            returns
+          </span>
+          <span classname='header_optionLineTwo'>
+            & orders
+          </span>
+        </div>
+        <div className='header_option'>
+          <span classname='header_optionLineOne'>
+            Your
+          </span>
+          <span classname='header_optionLineTwo'>
+            Prime
+          </span>
+        </div>
+      </div>
+
     </div>
   )
 }
