ef5f5fb2ecd5bc2217634252372006fa1c4a736d
Hamid Nur <hamidsharif2001@gmail.com>
2022-03-23 15:34:49 -0400

    update login

--- src/Header.js
+++ src/Header.js
@@ -27,11 +27,13 @@ function Header() {
       </div>
 
       <div className='header__nav'>
-        <div className='header__option'>
-          <span className='header__optionLineTwo'>
-            Sign In
-          </span>
-        </div>
+        <Link to='/login'>
+          <div className='header__option'>
+            <span className='header__optionLineTwo'>
+              Sign In
+            </span>
+          </div>
+        </Link>
 
         <div className='header__option'>
           <span className='header__optionLineTwo'>
