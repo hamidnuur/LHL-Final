fc60a433edd51d5f253a30c1d72d631ff7e3e353
Hamid Nur <hamidsharif2001@gmail.com>
2022-03-24 17:06:31 -0400

    implement payment functionality

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: '...' // The API URL
+  baseURL: 'http://localhost:5001/lhl-final-82e6c/us-central1/api' // The API URL
 });
 
 export default instance;
