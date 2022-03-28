2eebeb6d4376be3991d5dfa20e79d71046e8153e
Hamid Nur <hamidsharif2001@gmail.com>
2022-03-28 15:32:28 -0400

    Deploy app to firebase

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: 'http://localhost:5001/lhl-final-82e6c/us-central1/api' // The API URL
+  // The API URL
+  baseURL: 'https://us-central1-lhl-final-82e6c.cloudfunctions.net/api'
+  // 'http://localhost:5001/lhl-final-82e6c/us-central1/api'
 });
 
 export default instance;
