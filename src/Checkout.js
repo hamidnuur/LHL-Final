6832b54b7a5133a6dd6bf7103667b77c6696e063
Hamid Nur <hamidsharif2001@gmail.com>
2022-03-31 14:07:39 -0400

    update styling

--- src/Checkout.js
+++ src/Checkout.js
@@ -12,7 +12,7 @@ function Checkout() {
         <div className="checkout-left">
           <img
             className="checkout-ad"
-            src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+            src="./images/checkout-banner.png"
             alt=""
           />
   
